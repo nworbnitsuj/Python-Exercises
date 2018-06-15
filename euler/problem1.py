0e7f5f548a713d392758aaaad763e846dc57b58e
Justin Brown <justin.v.brown@gmail.com>
2018-06-14 20:43:33 -0500

    Reset problem1.py answer

--- euler/problem1.py
+++ euler/problem1.py
@@ -1,9 +1,7 @@
-for n in range(100):
-    if n % 3 == 0 and n % 5 == 0:
-        print('FizzBuzz')
-    elif n % 3 == 0:
-        print('Fizz')
-    elif n % 5 == 0:
-        print('Buzz')
-    else:
-        print(n)
+total = 0
+
+for n in range(1000):
+    if n % 3 == 0 or n % 5 == 0:
+        total += n
+
+print(total)
